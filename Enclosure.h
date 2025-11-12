0e6046b1ab750bb8caa1c021ac9d1c2c5f0aa457
noureldin1098-bit <noureldin1098@gmail.com>
2025-11-12 15:15:38 +0200

    Update Enclosure.h completed

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,27 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+//define #ifndef ENCLOSURE_H
+#define ENCLOSURE_H
+
+#include "Animal.h"
+
+class Enclosure {
+private:
+    Animal** animals;
+    int capacity;
+    int currentCount;
+
+public:
+    Enclosure(int cap = 5);
+    ~Enclosure();
+
+    void addAnimal(Animal* a);
+    void displayAnimals() const;
+};
+
+#endif
+enclosure class here 
 
 
 
