76d23a443ca3acb091e6641a9d94d8aef2d87319
noureldin1098-bit <noureldin1098@gmail.com>
2025-11-12 15:19:16 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -4,6 +4,19 @@
 #include "Animal.h"
 
 //define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+
+public:
+    Reptile();
+    Reptile(string n, int a, bool hungry, bool venom);
+    ~Reptile();
+
+    void display() const override;
+};
+
+#endif
 
 
 
