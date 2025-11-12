1f93e51b0a9889298dbda08398a8e5ba15239299
noureldin1098-bit <noureldin1098@gmail.com>
2025-11-12 15:19:43 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -4,7 +4,28 @@
 #include <string>
 using namespace std;
 
-//define visitor class here
+//define vi#ifndef VISITOR_H
+#define VISITOR_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+
+public:
+    Visitor();
+    Visitor(string name, int tickets);
+    ~Visitor();
+
+    void displayInfo() const;
+};
+
+#endif
+sitor class here
 
 
 
